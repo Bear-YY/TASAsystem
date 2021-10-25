c916384e06c0a989cbd9defc4f6e0a489aa260ce
Y Kumagai <k18rs042@st.kyusan-u.ac.jp>
2021-10-25 22:47:53 +0900

    全体ホーム画面を作成中(未完)

--- src/pb_home.php
+++ src/pb_home.php
@@ -20,22 +20,37 @@
 ?>
 
 <h2><b>本システムの概要</b></h2>
+<h6>推奨ブラウザ</h6>
+<ul>
+	<li>GoogleChrome</li>
+	<li>Firefox</li>
+</ul>
 <p>本システムは、TA・SAの募集、応募に関する情報を管理することで<br>
 TA・SAを頼みたい教員は募集情報を学生に伝え、学生は気軽に情報を得られるようにします。</p>
 
 <h4><b>教員が利用可能な機能</b></h4>
 <ul>
-	<li>募集可能時間割一覧</li>
-	<li>募集情報登録</li>
+	<li class="listitem">担当時間割一覧
+		<ul>
+			<img src="../img/teacher/home_1.png" width="60%" class="img-thumbnail" alt="Responsive image">
+		</ul>
+	</li>
+
+	<li class="listitem">募集情報登録
 	<li>応募・推薦者一覧</li>
-	<li>応募・推薦者詳細確認</li>
-	<li>TA・SA候補者推薦</li>
+		<ul>
+			<img src="../img/teacher/application_list_1.png" width="60%" class="img-thumbnail" alt="Responsive image">
+		</ul>
+	</li>
+	<li class="listitem">応募・推薦者詳細確認</li>
+	<li class="listitem">TA・SA候補者推薦</li>
 </ul>		
 
 <h4><b>学生が利用可能な機能</b></h4>
 <ul>
-	<li>募集中時間割の確認
+	<li class="listitem">募集中時間割の確認
 		<ul>
+			<img src="../img/student/home_1.png" width="60%" class="img-thumbnail" alt="Responsive image">
 			<li>
 				<span class="badge badge-success">▼カテゴリー相性：良!!</span>
 			：学生の履修科目の成績からカテゴリーごとの成績を算出し、良い成績順に、TA・SA募集中の時間割がお勧めとして表示されます。</li>
@@ -46,14 +61,27 @@ TA・SAを頼みたい教員は募集情報を学生に伝え、学生は気軽�
 			</li>
 		</ul>
 	</li>
-	<li>応募登録機能</li>
-	<li>スケジュールの登録・削除
+	<li class="listitem">応募登録機能
+	<li class="listitem">スケジュールの登録・削除
 		<ul>
+			<li>スケジュール登録の登録が行えます。スケジュールが入っていた場合は、応募可能科目の背景が赤色になり、応募出来なくなります。</li>
 		</ul>
 	</li>
-	<li>アンケート回答・編集</li>
-	<li>推薦状態の確認</li>
-	<li>推薦の回答</li>
+	<li class="listitem">アンケート回答・編集
+		<ul>
+			<li>アンケートに回答することによって、TA・SAを募集している時間割の中から資質の面で相性の良い科目を知らせてくれます。</li>
+		</ul>
+	</li>
+	<li class="listitem">推薦状態の確認
+		<ul>
+			<li>推薦が来ていた場合は、サイドバーにお知らせが届きます。</li>
+		</ul>
+	</li>
+	<li class="listitem">推薦の返答
+		<ul>
+			<li>推薦が来ている時間割に対して教員に、了承するか断るかを選択して応募してください。</li>
+		</ul>
+	</li>
 </ul>		
 
 </div>
