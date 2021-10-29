06bf45dcaf059e4ed9cb580c4cceefd63a4d34f9
Y Kumagai <k18rs042@st.kyusan-u.ac.jp>
2021-10-29 18:45:18 +0900

    学生の応募撤回処理は出来た

--- src/student_application.php
+++ src/student_application.php
@@ -5,6 +5,7 @@ require_once('data.php');
 $usr_id = $_SESSION['usr_id'];
 $stu_id = strtoupper($usr_id); //全部大文字
 $stu_id = mb_substr($stu_id, 1);  //頭の一文字を消す(細かい使い方は調べましょう)
+$act = 'insert';
 
 $rec_id = $_GET['rec_id'];
 $sql = <<<EOM
@@ -76,30 +77,40 @@ $row = $rs->fetch_assoc();
           </tr>
         </tbody>
       </table>
-      <hr color="#000000" width="80%" size="3">
-
-
-
 <?php 
 $sql = "SELECT * FROM tb_application WHERE stu_id = '{$stu_id}' AND rec_id = '{$rec_id}' LIMIT 1";
 $rs = $conn->query($sql);
 $row = $rs->fetch_assoc();
 
-if($row){
+if($row):
+  $act = 'update';
+?>
+  <h4>あなたはすでにこの時間割に応募しています。</h4>
+  <h4>結果がホーム画面に通知されるのでお待ちください。</h4>
+  <hr color="#000000" width="80%" size="3">
+<?php if($row['app_result'] == '3'):?>  
+    <p class="red">あなたはこの時間割のTA・SA募集を撤回しました。</p>
+<?php else: ?>
+    <p class="red">もし、諸事情でやむを得なく応募を撤回する場合は、撤回理由を明記の上、撤回を行ってください。</p>
+  <form action="?do=student_application_add&act=<?= $act ;?>" method="post" class="needs-validation" novalidate>
+    <input type="hidden" class="form-control" name="app_id" value="<?= $row['app_id']; ?>">
+    <div class="form-group">
+        <label for="app_cancmnt">応募撤回理由記入欄</label>
+        <textarea class="form-control" id="app_cancmnt" name='app_cancmnt' rows="4" required></textarea>
+        <div class="invalid-feedback">
+          テキストエリアに文章を入力してください。
+        </div>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">応募撤回する</button>
+  </form>
+<?php endif; ?>
 
-  echo '<h4>あなたはすでにこの時間割に応募しています。</h4>';
-  echo '<h4>結果がホーム画面に通知されるのでお待ちください。</h4>';
-
- ?>
-<?php 
-}else{
- ?>
-      <form action="?do=student_application_add" method="post" class="needs-validation" novalidate>
+<?php else: ?>
+      <form action="?do=student_application_add&act=<?= $act ;?>" method="post" class="needs-validation" novalidate>
       <div class="form-group">
           <!-- hiddenで(rec_id)を送るのはセキュリティ上どうなのだろうか？--ページのソース参照からばれてしまう。 -->
-          <?php 
-          echo '<input type="hidden" class="form-control" name="rec_id" value="'.$rec_id.'">'
-           ?>
+        <input type="hidden" class="form-control" name="rec_id" value="<?= $rec_id ?>">
       </div>
       <div class="form-group">
           <label for="app-comment">意気込み・連絡事項</label>
@@ -108,13 +119,10 @@ if($row){
             テキストエリアに文章を入力してください。
           </div>
       </div>
-      
       <button type="submit" class="btn btn-primary">応募する</button>
     </form>   
     </div>     
 </div>
 
-<?php 
-} 
-?>
+<?php endif; ?>
 <script src="../js/validation.js"></script>
--- src/student_application_add.php
+++ src/student_application_add.php
@@ -3,23 +3,34 @@ require_once('db_inc.php');
 $usr_id = $_SESSION['usr_id'];
 $stu_id = strtoupper($usr_id); //全部大文字
 $stu_id = mb_substr($stu_id, 1);  //頭の一文字を消す(細かい使い方は調べましょう)
+$act = $_GET['act'];
 
-$rec_id = $_POST['rec_id'];
 $date = strtotime("+12 year", time());   //日付を取得(12年後を取得)
 $app_day = date('Y-m-d', $date);		 //表示形式を'Y-m-d'(year-month-day)にする
-$app_comment = $_POST['app_comment'];
 
-echo $stu_id;
-echo $rec_id;   //日付を取得(12年後を取得)
-echo $app_day;		 //表示形式を'Y-m-d'(year-month-day)にする
-echo $app_comment;
+// echo $stu_id;
+// echo $rec_id;   //日付を取得(12年後を取得)
+// echo $app_day;		 //表示形式を'Y-m-d'(year-month-day)にする
+// echo $app_comment;
+echo $act;
+if($act == 'insert'){    
+    $app_comment = $_POST['app_comment'];
+    $rec_id = $_POST['rec_id'];
+    $sql = <<<EOM
+    INSERT INTO tb_application(stu_id,rec_id,app_day,app_comment) 
+    VALUES('{$stu_id}','{$rec_id}','{$app_day}','{$app_comment}')
+    EOM;
+}
+if($act == 'update'){
+    $app_cancmnt = $_POST['app_cancmnt'];
+    $app_id = $_POST['app_id'];
+    echo $app_cancmnt; echo '<br>';
+    echo $app_id; echo '<br>';    
 
-$sql = <<<EOM
-INSERT INTO tb_application(stu_id,rec_id,app_day,app_comment) 
-VALUES('{$stu_id}','{$rec_id}','{$app_day}','{$app_comment}')
-EOM;
+    $sql = "UPDATE tb_application SET app_cancmnt = '{$app_cancmnt}', app_result = '3' WHERE app_id = '{$app_id}'";
+}
 
 $rs = $conn->query($sql);
 $url = '?do=student_home';
 header('Location:'.$url);
- ?>
+?>
