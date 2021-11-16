d9a2dffe0d2b71aef306234c722d1070b7d33015
Y Kumagai <k18rs042@st.kyusan-u.ac.jp>
2021-11-16 16:12:09 +0900

    科目は配列に入れてから処理するように変更

--- src/admin_subject_list.php
+++ src/admin_subject_list.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // アクセス権限
 if (!isset($_SESSION['usr_kind'])){
 	die ('<h3>エラー：この機能はログインしないと利用できません</h3>');
@@ -10,11 +10,24 @@ if (!isset($_SESSION['usr_kind'])){
 require_once('db_inc.php');
 $sql = <<< EOM
 	SELECT * FROM tb_subject WHERE dpt_id = 'RS'
-	EOM;
+EOM;
 
 $rs = $conn->query($sql);
 if(!$rs) die('エラー: '. $conn->error);
 $row = $rs->fetch_assoc();
+$subjects = [];
+while($row){
+	$sub_id = $row['sub_id'];
+	$subjects[$sub_id] = [
+		'sub_name' => $row['sub_name'],
+		'dpt_id' => $row['dpt_id'],
+		'sub_unit' => $row['sub_unit'],
+		'get_year' => $row['get_year'],
+		'sub_section' => $row['sub_section']
+	];
+	$row = $rs->fetch_assoc();
+}
+
 ?>
 
 <article>
@@ -34,36 +47,30 @@ $row = $rs->fetch_assoc();
 </thead>
 <tbody>
 
-<?php
-while($row){
-?>
-
+<?php foreach ($subjects as $key => $value) :?>
 <tr>
 <th scope="row">
-<?php 
+<?php
 //echo '<a href="https://getbootstrap.com/docs/4.5/content/tables/">';  //1.
-echo $row['sub_name'].'</th>';
+echo $value['sub_name'].'</th>';
 //echo '</a>';															//2. 2つでリンクをつけられる
-echo '<td>'.$row['dpt_id'].'</td>';
-echo '<td>'.$row['sub_unit'].'</td>';
+echo '<td>'.$value['dpt_id'].'</td>';
+echo '<td>'.$value['sub_unit'].'</td>';
 $select1 = array(1 => "1年生",
 			2 => "2年生",
 			3 => "3年生",
 			4 => "4年生",);
-echo '<td>'.$select1[$row['get_year']].'</td>';
+echo '<td>'.$select1[$value['get_year']].'</td>';
 $select2 = array(1 => "必修科目",
 			2 => "選択科目",
 			3 => "導入科目",
 			4 => "教養科目");
-echo '<td>'.$select2[$row['sub_section']].'</td>';
+echo '<td>'.$select2[$value['sub_section']].'</td>';
 
 ?>
-<td align="center"><a class="btn btn-secondary btn-sm" href="?do=admin_timetable&sub_id=<?= $row['sub_id'];?>" role="button">時間割登録</a></td>
+<td align="center"><a class="btn btn-secondary btn-sm" href="?do=admin_timetable&sub_id=<?= $key;?>" role="button">時間割登録</a></td>
 </tr>
-<?php
-$row = $rs->fetch_assoc ();  
-}
-?>
+<?php endforeach; ?>
 </tbody>
 </table>
 </div>
